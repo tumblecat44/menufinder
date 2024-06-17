800fbdaecfd8355414baf7ab334aea4050367249
dlrjsgml44 <leegeh1213@gmail.com>
2024-06-17 20:56:52 +0900

    날짜 에딧텍스트 말고 버튼으로 바꾸고 날짜 바꾸는거 java.time으로 바꿈

--- app/src/main/java/com/dlrjsgml/bobeat/MainActivity.kt
+++ app/src/main/java/com/dlrjsgml/bobeat/MainActivity.kt
@@ -1,39 +1,61 @@
 package com.dlrjsgml.bobeat
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.dlrjsgml.bobeat.data.MealResponse
 import com.dlrjsgml.bobeat.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding : ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    var mutableDate : LocalDateTime = LocalDateTime.now()
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.letsgo.setOnClickListener {
-            if(binding.writens.text.isNotEmpty()){
-                try {
-                    getMealInfo("${binding.writens.text}")
-                    binding.whatday.text = "${binding.writens.text.toString().substring(0 until 4)}년${binding.writens.text.toString().substring(4 until 6)}월${binding.writens.text.toString().substring(6 until 8)}일 의 급식은?"
-                }
-                catch(e : Exception){
-                    binding.whatday.text = "입력 형식이 틀렸습니다."
-                }
-            }
+        initDo()
+        binding.yesterday.setOnClickListener {
+            changeDay(-1)
         }
-
+        binding.tommorow.setOnClickListener {
+            changeDay(1)
+        }
     }
-
-    private fun getMealInfo(whens : String) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initDo(){
+        getMealInfo(mutableDate!!.format(DateTimeFormatter.ofPattern("yyyyMMdd")))
+        val datetoStringMan = mutableDate!!.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+        val monthman =datetoStringMan.substring(4, 6).toInt().toString()
+        val dayman = datetoStringMan.substring(6, 8).toInt().toString()
+        binding.whatday.text = "${datetoStringMan.substring(0,4)}년 ${monthman}월 ${dayman}일의 급식은?"
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun changeDay(good : Long){
+        getMealInfo(mutableDate!!.plusDays(good).format(DateTimeFormatter.ofPattern("yyyyMMdd")))
+        mutableDate = mutableDate!!.plusDays(good)
+        val datetoStringMan = mutableDate!!.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+        val monthman =datetoStringMan.substring(4, 6).toInt().toString()
+        val dayman = datetoStringMan.substring(6, 8).toInt().toString()
+        binding.whatday.text = "${datetoStringMan.substring(0,4)}년 ${ monthman}월 ${ dayman}일의 급식은?"
+    }
+    private fun getMealInfo(whens: String) {
         val call = RetrofitInstance.api.getMealInfo(
-            key =  "07712bb365424a558eab0a7ece4638c1",
+            key = "07712bb365424a558eab0a7ece4638c1",
             type = "json",
             pIndex = 1,
             pSize = 100,
@@ -49,22 +71,19 @@ class MainActivity : AppCompatActivity() {
                     mealResponse?.let {
                         val menuContents = mealResponse.toString()
                         // Handle the response here
-                        try{
-                            binding.first.text = menuContents.split(",")[1]
-                        }
-                        catch (e:Exception){
+                        try {
+                            binding.first.text = menuContents.split(",")[1].replace("<br/>","\n").substring(39,)
+                        } catch (e: Exception) {
                             binding.first.text = "오늘 아침이 없습니다."
                         }
-                        try{
-                            binding.sec.text = menuContents.split(",")[2]
-                        }
-                        catch (e:Exception){
+                        try {
+                            binding.sec.text = menuContents.split(",")[2].replace("<br/>","\n").substring(14,)
+                        } catch (e: Exception) {
                             binding.sec.text = "오늘 점심이 없습니다."
                         }
-                        try{
-                            binding.third.text = menuContents.split(",")[3]
-                        }
-                        catch (e:Exception){
+                        try {
+                            binding.third.text = menuContents.split(",")[3].replace("<br/>","\n").substring(14,)
+                        } catch (e: Exception) {
                             binding.third.text = "오늘 저녁이 없습니다."
                         }
 
